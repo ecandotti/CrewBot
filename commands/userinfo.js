6f1b9e9c601fa92675c17092781a1b7094163ec8
Enzo Candotti <enzo.candotti16@hotmail.com>
2020-11-22 17:48:30 +0100

    Delete userinfo.js

--- commands/userinfo.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "userinfo",
-    description: "Donne des infos sur l'utilisateur",
-    execute(message,args) {
-        const user_mention = message.mentions.users.first()
-        message.channel.send(`Voici le tag de la personne mentionné : ${user_mention.tag}`)
-    }
-}
