be2ad7268860545356c55e4201be78d91a22388d
ecandotti <enzo.candotti16@hotmail.com>
2020-11-23 12:42:16 +0100

    Function which show queue

---
+++ commands/show.js
@@ -0,0 +1,18 @@
+let { myqueue } = require('../config.json')
+
+module.exports = {
+    name: "show",
+    description: "Show current queue",
+    async execute(message) {
+        
+        if(myqueue.length <= 0) {
+            message.channel.send("La file d'attente est vide")
+        }   
+        else {
+            message.channel.send("Voici la file d'attente :")
+            for(var i= 0; i < myqueue.length; i++) {
+                message.channel.send(String(myqueue[i]))
+            }
+        }
+    }
+}
