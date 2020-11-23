f943f5667f69bc7a96d1e9d55c065b40228573f4
ecandotti <enzo.candotti16@hotmail.com>
2020-11-23 12:40:52 +0100

    Add Stop function

---
+++ commands/stop.js
@@ -0,0 +1,23 @@
+let { myqueue } = require('../config.json')
+
+module.exports = {
+    name: "stop",
+    description: "Bot musique",
+    execute(message) {
+        if(message.member.voice.channel) {
+            message.member.voice.channel.join()
+                .then(connexion => {
+                    let dispatcher = connexion.play('rien')
+                    dispatcher.destroy()
+                    connexion.disconnect()
+                    myqueue = []
+                    message.channel.send("File d'attente nettoyé")
+                })
+                .catch(err => {
+                    message.channel.send(`Une erreur est survenu : ${err}`)
+                })
+        } else {
+            message.channel.send("Vous devez êtres présent sur un salon vocal ! Banane !")
+        }
+    }
+}
