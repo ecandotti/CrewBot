08a40b1a1e0a3eeaa5b97b0e1caa139f09fe5c28
ecandotti <enzo.candotti16@hotmail.com>
2020-12-07 01:59:46 +0100

    Clean code

--- commands/help.js
+++ commands/help.js
@@ -1,13 +1,13 @@
-const { prefix, exist_command } = require("../config.json")
+const { prefix, help_com } = require('../config.json');
 
 module.exports = {
-    name: "help",
-    description: "Donne les commandes existantes",
-    howUse: `${prefix}help`,
-    async execute(message) {
-        message.channel.send(`Préfix utilisé : ${prefix}`)
-        for(var i= 0; i < exist_command.length; i++) {
-            message.channel.send(String(exist_command[i]))
-        }
-    }
-}
+	name: 'help',
+	description: 'Show commands',
+	howUse: `${prefix}help`,
+	async execute(message) {
+		message.channel.send(`Prefix used : ${prefix}`);
+		for(let i = 0; i < help_com.length; i++) {
+			message.channel.send(String(help_com[i]));
+		}
+	},
+};
